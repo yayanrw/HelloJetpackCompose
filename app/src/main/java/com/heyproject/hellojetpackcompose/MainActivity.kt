9b94780dc8893e4acadfed3f49249889a34c024e
yayanrw <yayanraw@gmail.com>
2022-11-01 13:07:17 +0700

    Initial commit

---
+++ app/src/main/java/com/heyproject/hellojetpackcompose/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.heyproject.hellojetpackcompose
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.heyproject.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
+
+private val sampleName = listOf(
+    "Andre",
+    "Desta",
+    "Parto",
+    "Wendy",
+    "Komeng",
+    "Raffi Ahmad",
+    "Andhika Pratama",
+    "Vincent Ryan Rompies"
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloJetpackComposeTheme {
+                HelloJetpackComposeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloJetpackComposeApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+    ) {
+        GreetingList(sampleName)
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun HelloJetpackComposeAppPreview() {
+    HelloJetpackComposeTheme {
+        HelloJetpackComposeApp()
+    }
+}
+
+@Composable
+fun GreetingList(names: List<String>) {
+    if (names.isNotEmpty()) {
+        Column {
+            for (name in names) {
+                Greeting(name)
+            }
+        }
+    } else {
+        Text("No people to great :(")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(R.drawable.jetpack_compose),
+            contentDescription = "Logo jetpack compose",
+            modifier = Modifier.size(80.dp)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Column() {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                )
+            Text(text = "Welcome to Jetpack Compose")
+        }
+    }
+}
